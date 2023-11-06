c47b7fba9c56f99646beb690d667e52a7edce1b9
CookieJunkie0 <rtrt96169@gmail.com>
2023-11-06 16:50:38 +0500

    main

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
 require('dotenv').config({path: __dirname + '/.env'});
 const ethers = require('ethers');
+const axios = require('axios');
 const fs = require('fs');
 const { ATESTATOR_ABI } = require('./abis');
+const { shuffle } = require('./helper');
 
-const MANTLE_RPC = process.env.MANTLE_RPC;
-const MANTLE_PROVIDER = new ethers.providers.JsonRpcProvider(MANTLE_RPC);
+const MIX_WALLETS = JSON.parse(process.env.MIX_WALLETS);
+const MANTLE_PROVIDER = new ethers.providers.JsonRpcProvider(process.env.MANTLE_RPC);
 
-const WALLETS = fs.readFileSync(__dirname + '/wallets.txt', 'utf8').split('\n');
+async function getAttestationData(wallet) {
+    try {
+        const response = await axios.post("https://gateway.clique.social/sbt-signer/attestor/64c9cd892fce35a476860fb5/signature", 
+        { walletAddress: wallet });
 
-function mintAtestat(account) {
-    const signer = account.connect(MANTLE_PROVIDER);
-    const contract = new ethers.Contract("0x7C78b18F496d3D37c44De09da4a5a76Eb34B7e74", ATESTATOR_ABI, signer);
+        if(response.status === 200) {
+            return {success: true, sig: response.data.signature, url: response.data.attestationUrl};
+        } else { return { success:false } }
+    } catch(e) {return {success: false, err: e}}
+}
 
-    
+getAttestationData("0xD253d1275008257AD21A48546f710DC7Ca8f378b").then(console.log)
+
+async function mintAtestat(account) {
+    try {
+        const contract = new ethers.Contract("0x7C78b18F496d3D37c44De09da4a5a76Eb34B7e74", ATESTATOR_ABI, signer);
+        const signer = account.connect(MANTLE_PROVIDER);    
+        
+        const attData = await getAttestationData(account.address);
+        if(!attData.success) return attData;
+
+        const tx = await contract.mintMJ(account.address, 1, 0, '', attData.url, attData.sig);
+        const receipt = await tx.wait();
+
+        if(receipt.status !== 1) { return {success: false, err: "TX failed", hash: receipt.transactionHash} }
+        return {success: true, hash: receipt.transactionHash};
+    } catch(e) {return {success: false, err: e}}  
+}
+
+async function main() {
+
+    let wallets = fs.readFileSync(__dirname + '/wallets.txt', 'utf8').split('\n');
+
+    MIX_WALLETS? wallets = shuffle(wallets) : '';
+
+    for (const wallet of wallets) {
+        const account = new ethers.Wallet(wallet, MANTLE_PROVIDER);
+        const mint = await mintAtestat(account);
+    }
 }
