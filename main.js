c24d7c385be81299f1a363e01c336c534c62028c
CookieJunkie0 <almo333k@yandex.ru>
2023-11-07 15:21:14 +0500

    1

--- main.js
+++ main.js
@@ -8,18 +8,60 @@ const { shuffle } = require('./helper');
 const MIX_WALLETS = JSON.parse(process.env.MIX_WALLETS);
 const MANTLE_PROVIDER = new ethers.providers.JsonRpcProvider(process.env.MANTLE_RPC);
 
+async function getSignatureMessage(wallet) {
+    try {
+        const response = await axios.get(`https://mdi-quests-api-production.up.railway.app/auth/web3/signature?address=${wallet}`);
+
+        if(response.status !== 200) {
+            return {success: false, err: response.data};
+        }
+        
+        return {success: true, msg: response.data.message};
+    } catch(e) { return { success: false, err: e } }
+}
+
+async function register(account, inviteCode) {
+    try {
+        const sigMessage = await getSignatureMessage(account.address);
+        const sig = await account.signMessage(sigMessage.msg);
+
+        const response = await axios.post(`https://mdi-quests-api-production.up.railway.app/auth/web3/login`, {
+            address: account.address,
+            inviteCode: inviteCode,
+            signature: sig,
+            walletType: "metamask"
+        });
+
+        if(response.status !== 201) {
+            return {success: false, err: response.data};
+        }
+
+        const refResponse = await axios.post(`https://mdi-quests-api-production.up.railway.app/referral/set-referrer`, { inviteCode: inviteCode }, {
+            headers: {
+                "Mdi-Jwt": `${response.data.web3Token}`
+            }
+        });
+
+        if(refResponse.status !== 200) {
+            return {success: false, err: refResponse.data};
+        }
+
+        return {success: true};
+    } catch(e) { return { success: false, err: e } }
+}
+
 async function getAttestationData(wallet) {
     try {
         const response = await axios.post("https://gateway.clique.social/sbt-signer/attestor/64c9cd892fce35a476860fb5/signature", 
         { walletAddress: wallet });
 
-        if(response.status === 200) {
-            return {success: true, sig: response.data.signature, url: response.data.attestationUrl};
-        } else { return { success:false } }
+        if(response.status !== 200) {
+            return {success: false, err: response.data};
+        }
+        
+        return {success: true, sig: response.data.signature, url: response.data.attestationUrl};
     } catch(e) {return {success: false, err: e}}
 }
-
-getAttestationData("0xD253d1275008257AD21A48546f710DC7Ca8f378b").then(console.log)
 
 async function mintAtestat(account) {
     try {
@@ -48,3 +90,5 @@ async function main() {
         const mint = await mintAtestat(account);
     }
 }
+
+register(new ethers.Wallet('0xf2b8983149fea6233bbbe08a5a927339808b6049de1ec034c6566d90615d59b5'), 'A9GRX1JPAI').then(console.log)
